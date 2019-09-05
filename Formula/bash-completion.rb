ed087df634d936ab1e48a931d9c842da2c67bd8d
Tim Visher <tim@stitchdata.com>
2019-09-05 15:38:51 -0400

    Rely on scop bash-completion

--- Formula/bash-completion.rb
+++ Formula/bash-completion.rb
@@ -2,9 +2,9 @@
 # with 3.2.57. If you've upgraded bash, use bash-completion@2 instead.
 class BashCompletion < Formula
   desc "Programmable completion for Bash 3.2"
-  homepage "https://salsa.debian.org/debian/bash-completion"
-  url "https://src.fedoraproject.org/repo/pkgs/bash-completion/bash-completion-1.3.tar.bz2/a1262659b4bbf44dc9e59d034de505ec/bash-completion-1.3.tar.bz2"
-  sha256 "8ebe30579f0f3e1a521013bcdd183193605dab353d7a244ff2582fb3a36f7bec"
+  homepage "https://github.com/scop/bash-completion"
+  url "https://github.com/scop/bash-completion/archive/1.3.tar.gz"
+  sha256 "e0edb2540cc7e8904a9e517983e8a2c3a27fc2d80f6210398fca2a1c093224ae"
   revision 3
 
   bottle do
@@ -18,17 +18,12 @@ class BashCompletion < Formula
 
   conflicts_with "bash-completion@2", :because => "Differing version of same formula"
 
-  # Backports the following upstream patch from 2.x:
-  # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=740971
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/c1d87451da3b5b147bed95b2dc783a1b02520ac5/bash-completion/bug-740971.patch"
-    sha256 "bd242a35b8664c340add068bcfac74eada41ed26d52dc0f1b39eebe591c2ea97"
-  end
-
-  # Backports (a variant of) an upstream patch to fix man completion.
-  patch :DATA
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
 
   def install
+    system "autoreconf", "-i"
+
     inreplace "bash_completion" do |s|
       s.gsub! "/etc/bash_completion", etc/"bash_completion"
       s.gsub! "readlink -f", "readlink"
@@ -48,15 +43,3 @@ class BashCompletion < Formula
     system "bash", "-c", ". #{etc}/profile.d/bash_completion.sh"
   end
 end
-__END__
---- a/completions/man
-+++ b/completions/man
-@@ -27,7 +27,7 @@
-     fi
-
-     uname=$( uname -s )
--    if [[ $uname == @(Linux|GNU|GNU/*|FreeBSD|Cygwin|CYGWIN_*) ]]; then
-+    if [[ $uname == @(Darwin|Linux|GNU|GNU/*|FreeBSD|Cygwin|CYGWIN_*) ]]; then
-         manpath=$( manpath 2>/dev/null || command man --path )
-     else
-         manpath=$MANPATH
